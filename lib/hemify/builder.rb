8cdc1152ed08fb2ff0e44b0518ce7910d14c1849
Kareem Kouddous <kareemk@crowdtap.com>
2012-01-09 14:12:28 -0500

    Recursive copy to handle sub-directories

--- lib/hemify/builder.rb
+++ lib/hemify/builder.rb
@@ -14,7 +14,7 @@ module Hemify
               `cp public/application.css #{Rails.root}/vendor/assets/stylesheets/#{project}.css`
               `rm -rf #{Rails.root}/vendor/assets/images/#{project}`
               `mkdir -p #{Rails.root}/vendor/assets/images/#{project}`
-              `cp public/#{project}/* #{Rails.root}/vendor/assets/images/#{project}`
+              `cp -r public/#{project}/* #{Rails.root}/vendor/assets/images/#{project}`
             end
           end
         end
